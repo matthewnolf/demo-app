a534dae2c0e4d09660e85b65b2c34d7210b39a8b
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2021-12-09 18:32:07 +0100

    Set revision to current

--- service/service.go
+++ service/service.go
@@ -9,7 +9,7 @@ import (
 
 func DoService() {
 	for {
-		l := logger.Logger("github.com/matthewnolf/demo-app", "b07bc36553512dc443072d805536a119b64dca72")
+		l := logger.Logger("github.com/matthewnolf/demo-app", "8f348bb348e35b670169b7fe1a26ac9ff97cb7ba")
 		level.Info(l).Log("different", "branch")
 
 		time.Sleep(5 * time.Second)
